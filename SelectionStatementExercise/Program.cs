c2cbc42af7ed63cea43a4ae5dcf27e0cd7b1eceb
maxwell1565 <146258976+maxwell1565@users.noreply.github.com>
2024-02-19 14:32:37 -0600

    exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int favNumber = 13;
+            Console.WriteLine("Try to guess my favorite number.");
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput >= 14)
+            {
+                Console.WriteLine("Your guess is too high!");
+
+            }
+            else if (userInput == 13)
+            {
+                Console.WriteLine("You're correct!");
+            }
+            else if (userInput <= 12)
+            {
+                Console.WriteLine("your guess is too low!");
+            }
+            else
+            {
+                Console.WriteLine("Nevermind.");
+            }
         }
     }
 }
